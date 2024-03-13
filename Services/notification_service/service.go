92498ed17c0f639809372fda2effa84066de7064
Ankita Dewari <ankita.dewari10@gmail.com>
2024-03-13 21:31:13 +0530

    new branch

---
+++ Services/notification_service/service.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"bytes"
+	"consumer"
+	"fmt"
+	"net/http"
+)
+
+func Deliver(event consumer.Event, notificationType string) error {
+	switch notificationType {
+	case "webhook":
+		client := &http.Client{}
+
+		req, err := http.NewRequest("POST", event.DestinationAddress, bytes.NewBuffer(event.Message))
+		if err != nil {
+			fmt.Print("error creating http req", err)
+			return err
+		}
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Print("error sending msg to webhook", err)
+			return err
+		}
+		defer resp.Body.Close()
+		fmt.Print("msg sent", resp.Status)
+
+	default:
+		fmt.Print("notification type required", notificationType)
+
+	}
+	return nil
+}
+
+func Automate() {
+	brokeraddress := []string{"localhost:9092"}
+	consumerRecieved := consumer.NewReader(brokeraddress, "quickstart-events")
+	for {
+		consumedEvent := consumerRecieved.Consume()
+		err := Deliver(consumedEvent, consumedEvent.NotificationType)
+		if err != nil {
+			fmt.Print("err in producing msg", err)
+			return
+		}
+	}
+
+}
